5d328c29e19a7c72ede28a12f1d183af284aa57d
Daniel <daniel@translucentcomputing.com>
2016-05-31 23:47:44 -0300

    inicio

---
+++ app/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Daniel Costa <danielcosta123@gmail.com> on 11/20/2015.
+ */
+
+'use strict';
+
+angular.module('MyApp', ['ui.bootstrap'])
+    .constant('_', window._); //lodash
+
+angular.module('MyApp')
+    .controller('AppController', function ($scope) {
+        
+
+    });
---
+++ server.js
@@ -0,0 +1,34 @@
+/**
+ * Created by User on 5/31/2016.
+ */
+var express = require('express');
+var snmp = require("net-snmp");
+var StringDecoder = require('string_decoder').StringDecoder;
+
+var app = express();
+app.listen(8080);
+console.log("App listening on port 8080");
+
+app.get('/api/test', function (req, res) {
+
+    var decoder = new StringDecoder('utf8');
+
+    var session = snmp.createSession("127.0.0.1", "public", {version: snmp.Version2c});
+
+    var oids = ['1.3.6.1.2.1.1.5.0', '1.3.6.1.2.1.1.6.0'];
+
+    session.get(oids, function (err, result) {
+        if (err) {
+            console.log(err);
+        }else{
+            for (var i = 0; i < result.length; i++) {
+                if(result[i].type == 4){
+                    //octet string
+                    result[i].value = decoder.write(result[i].value);
+                }
+            }
+            res.json(result);
+        }
+    });
+
+});
